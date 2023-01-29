1a918948094a23ac75f372577f41d3d440266fc4
capmahin <mahinulhuque1996@gmail.com>
2023-01-30 00:50:48 +0600

    add useRouter()

--- pages/fruit/[name]/[subname].tsx
+++ pages/fruit/[name]/[subname].tsx
@@ -1,4 +1,12 @@
+import { useRouter } from "next/router"
+
 export default function FruitName() {
+
+    const router = useRouter()
+
+    console.log(router)
+
+
     return <h1>Hello from 1st segment</h1>
 }
 
