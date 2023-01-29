2fc0b371e2d7e8fddb1634d391df8de1aa17b566
capmahin <mahinulhuque1996@gmail.com>
2023-01-29 17:09:53 +0600

    dynamic route

---
+++ pages/fruit/[name]/[subname].tsx
@@ -0,0 +1,5 @@
+export default function FruitName() {
+    return <h1>Hello from 1st segment</h1>
+}
+
+//http://localhost:3000/fruit/hlww
---
+++ pages/fruit/[name]/index.tsx
@@ -0,0 +1,5 @@
+export default function FruitName() {
+    return <h1>Hello </h1>
+}
+
+//http://localhost:3000/fruit/hlww
