46e7482efedc675597d9687b76405efbf0d80448
capmahin <mahinulhuque1996@gmail.com>
2023-01-31 01:32:15 +0600

    add getStaticPath

---
+++ pages/fruit/[name].tsx
@@ -0,0 +1,33 @@
+import { useRouter } from "next/router";
+
+import { GetStaticPaths, GetStaticProps } from "next"
+import path from 'path'
+
+export const getStaticProps: GetStaticProps = async (context) => {
+
+    const fs = require('fs')
+
+
+
+    return {
+        revalidate: 10,
+        props: {
+            myFavNum: Math.random()
+        }
+    }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [],
+        fallback: false
+    }
+}
+
+export default function MyFruit(props) {
+    const router = useRouter()
+
+
+
+    return <h1>Hello{props.myFavNum}</h1>
+}
